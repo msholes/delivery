f2103b02158dc0f79e22e48dabb8390ec08ce1ad
Michael Sholes <msholes06@gmail.com>
2020-02-01 14:16:24 -0500

    adding html

--- main.py
+++ main.py
@@ -18,6 +18,15 @@ def bob():
     val = {"value": "bob"}
     return jsonify(val)
 
+@app.route('/html')
+def html():
+    """Returns some custom HTML"""
+    return """
+    <title>This is a Hello World World Page</title>
+    <p>Hello</p>
+    <p><b>World</b></p>
+    """
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
